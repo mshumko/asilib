904e232b7f16a056fb8256620f2594f1015899d6
Mykhaylo <msshumko@gmail.com>
2021-01-10 18:53:21 -0800

    Added the download wrapper.

--- asi/__init__.py
+++ asi/__init__.py
@@ -1,4 +1,8 @@
 # Currently the download scripts can be accessed by 
 # asi.download_themis.download_asi_frames,
 # but I want to just have asi.download_themis() function.
-from .download import download_themis
+# from .download import download_themis
+
+from asi.download.download_wrapper import download # Import download scrips.
+# from . import plot_frame
+# from . import plot_movie
---
+++ asi/download/download_wrapper.py
@@ -0,0 +1,4 @@
+from . import download_themis
+
+def download():
+    print('in download')
