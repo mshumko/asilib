cfeee074e87df29655828d3448d1596e483e0e48
Mykhaylo Shumko <mykhaylo.shumko@nasa.gov>
2021-01-22 11:52:43 -0800

    Removed config.py from git.
    Added an ImportError in __init__.py if config.py file is
    not found and the user needs to run aurora_asi as a
    module.

--- asi/__init__.py
+++ asi/__init__.py
@@ -3,15 +3,15 @@
 # but I want to just have asi.download_themis() function.
 # from .download import download_themis
 import warnings
+import pathlib
+
+# Check that the package has been configured and config.py file exists.
+here = pathlib.Path(__file__).parent.resolve()
+if not pathlib.Path(here / 'config.py').is_file():
+    raise ImportError('config.py file with the ASI data directory not found. '
+                'Did you run "python3 -m aurora_asi init"?')
 
 from asi.download.download_wrapper import download # Import download scrips.
 from asi.download import download_rego
 # from . import plot_frame
 # from . import plot_movie
-
-# Check that the package has been configured.
-try:
-    import asi.config
-except ModuleNotFoundError:
-    warnings.warn('config.py file with the data directory not found. '
-                'Did you run "python3 -m aurora_asi init"?')
--- asi/config.py
+++
@@ -1,4 +0,0 @@
-import pathlib
-
-PROJECT_DIR = pathlib.Path("/Users/mshumko/Documents/research/aurora_asi/asi")
-ASI_DATA_DIR = pathlib.Path("/Users/mshumko/Documents/research/aurora_asi/data")
