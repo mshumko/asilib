ecdc0be9d1e8ddab6d2e42f3c9f9d95579ad37fb
Mykhaylo Shumko <mykhaylo.shumko@nasa.gov>
2021-01-22 11:42:43 -0800

    Cleaned up the old code in download_rego.py
    I also added a check that config.py exists in __init__.py

--- asi/__init__.py
+++ asi/__init__.py
@@ -2,8 +2,16 @@
 # asi.download_themis.download_asi_frames,
 # but I want to just have asi.download_themis() function.
 # from .download import download_themis
+import warnings
 
 from asi.download.download_wrapper import download # Import download scrips.
 from asi.download import download_rego
 # from . import plot_frame
 # from . import plot_movie
+
+# Check that the package has been configured.
+try:
+    import asi.config
+except ModuleNotFoundError:
+    warnings.warn('config.py file with the data directory not found. '
+                'Did you run "python3 -m aurora_asi init"?')
--- asi/download/download_rego.py
+++ asi/download/download_rego.py
@@ -10,11 +10,73 @@ from bs4 import BeautifulSoup
 from asi import config
 
 """
-This program contains functions to download the Red-line Emission Geospace 
-Observatory (REGO) data from the https://data.phys.ucalgary.ca server.
+This program contains the download() function to download the Red-line Emission Geospace 
+Observatory (REGO) data from the https://data.phys.ucalgary.ca server to the 
+config.ASI_DATA_DIR/rego/ directory
 """
 
 BASE_URL = 'https://data.phys.ucalgary.ca/sort_by_project/GO-Canada/REGO/stream0/'
+
+
+def download(day: Union[datetime, str], station: str, download_minute: bool=True):
+    """
+    The wrapper to download the REGO data given the day, station name,
+    and a flag to download a single minute file or the entire hour. The images
+    are saved to the config.ASI_DATA_DIR / 'rego' directory. 
+
+    Parameters
+    ----------
+    day: datetime.datetime or str
+        The date and time to download the data from. If day is string, 
+        dateutil.parser.parse will attempt to parse it into a datetime
+        object.
+    station: str
+        The station id to download the data from.
+    download_minute: bool (optinal)
+        If True, will download only one minute of image data, otherwise it will
+        download image data from the entire hour.
+
+    Returns
+    -------
+    None
+
+    Example
+    -------
+    day = datetime(2017, 4, 13, 5, 10)
+    station = 'LUCK'
+    download(day, station)  # Will download to the aurora_asi/data/rego/ folder.
+    """
+    if isinstance(day, str):
+        day = dateutil.parser.parse(day)
+    # Add the year/month/day url folders onto the url
+    url = BASE_URL + f'{day.year}/{str(day.month).zfill(2)}/{str(day.day).zfill(2)}/'
+
+    # Find if the particular camera station was taking data on that day.
+    station_url = search_hrefs(url, search_pattern=station.lower())
+    # Append the station url directory and the UTC hour to the url.
+    url +=  f'{station_url[0]}ut{str(day.hour).zfill(2)}/'
+
+    # Make the REGO directory if doesn't exist.
+    if not pathlib.Path(config.ASI_DATA_DIR, 'rego').exists():
+        pathlib.Path(config.ASI_DATA_DIR, 'rego').mkdir()
+
+    if download_minute:
+        # Find an image file for the one minute.
+        file_names = search_hrefs(url, search_pattern=day.strftime('%Y%m%d_%H%M'))
+        # Download file
+        r = requests.get(url + file_names[0], allow_redirects=False)
+        with open(config.ASI_DATA_DIR / 'rego' / file_names[0], 'wb') as f:
+            f.write(r.content)
+    else:
+        # Otherwise find all of the image files for that station and UT hour.
+        file_names = search_hrefs(url)
+        # Download files
+        for file_name in file_names:
+            r = requests.get(url + file_name, allow_redirects=False)
+            with open(config.ASI_DATA_DIR / 'rego' / file_name, 'wb') as f:
+                f.write(r.content)
+    return
+
 
 def search_hrefs(url: str, search_pattern: str ='') -> List[str]:
     """
@@ -48,109 +110,3 @@ def search_hrefs(url: str, search_pattern: str ='') -> List[str]:
         raise NotADirectoryError(f'The url {url} does not contain any hyper '
             f'references containing the search_pattern="{search_pattern}".')
     return matched_hrefs
-
-
-def download(day: Union[datetime, str], station: str, download_minute: bool=True):
-    """
-    The wrapper to download the REGO data given the day, station name,
-    and a flag to download a single minute file or the entire hour. The images
-    are saved to the config.ASI_DATA_DIR / 'rego' directory. 
-
-    Parameters
-    ----------
-    day: datetime.datetime or str
-        The date and time to download the data from. If day is string, 
-        dateutil.parser.parse will attempt to parse it into a datetime
-        object.
-    station: str
-        The station id to download the data from.
-    download_minute: bool (optinal)
-        If True, will download only one minute of image data, otherwise it will
-        download image data from the entire hour.
-
-    Returns
-    -------
-    None
-    """
-    if isinstance(day, str):
-        day = dateutil.parser.parse(day)
-    # Add the year/month/day url folders onto the url
-    url = BASE_URL + f'{day.year}/{str(day.month).zfill(2)}/{str(day.day).zfill(2)}/'
-
-    # Find if the particular camera station was taking data on that day.
-    station_url = search_hrefs(url, search_pattern=station.lower())
-    # Append the station url directory and the UTC hour to the url.
-    url +=  f'{station_url[0]}ut{str(day.hour).zfill(2)}/'
-
-    # Make the REGO directory if doesn't exist.
-    if not pathlib.Path(config.ASI_DATA_DIR, 'rego').exists():
-        pathlib.Path(config.ASI_DATA_DIR, 'rego').mkdir()
-
-    if download_minute:
-        # Find an image file for the one minute.
-        file_names = search_hrefs(url, search_pattern=day.strftime('%Y%m%d_%H%M'))
-        # Download file
-        r = requests.get(url + file_names[0], allow_redirects=False)
-        with open(config.ASI_DATA_DIR / 'rego' / file_names[0], 'wb') as f:
-            f.write(r.content)
-    else:
-        # Otherwise find all of the image files for that station and UT hour.
-        file_names = search_hrefs(url)
-        # Download files
-        for file_name in file_names:
-            r = requests.get(url + file_name, allow_redirects=False)
-            with open(config.ASI_DATA_DIR / 'rego' / file_name, 'wb') as f:
-                f.write(r.content)
-    return
-
-# day = datetime(2017, 4, 13, 5, 10)
-# station = 'LUCK'
-# download(day, station)
-
-# url = (f'https://data.phys.ucalgary.ca/sort_by_project/GO-Canada/REGO/stream0/'
-#         f'{day.year}/{str(day.month).zfill(2)}/{str(day.day).zfill(2)}/')
-
-# print(url)
-
-# print(search_hrefs(url, search_pattern='luck'))
-
-# r = requests.get(url)
-# soup = BeautifulSoup(r.content, 'html.parser')
-
-# hrefs = soup.find_all('a', href=True)
-# print(hrefs)
-
-# for href in hrefs:
-#     if station.lower() in href.text:
-#         station_url = href.text
-#         break
-#     else:
-#         station_url = ''
-
-# if station_url == '':
-#     raise NotADirectoryError(f'The the directory for {station} not found in \n{url}.')
-
-# # 'https://data.phys.ucalgary.ca/sort_by_project/GO-Canada/REGO/stream0/2017/04/13/luck_rego-649/ut05/'
-
-# url2 = url + station_url + f'ut{str(day.hour).zfill(2)}/'
-
-# r = requests.get(url2)
-# soup = BeautifulSoup(r.content, 'html.parser')
-
-# hrefs = soup.find_all('a', href=True)
-
-
-# date_str = day.strftime('%Y%m%d_%H%M')
-# for href in hrefs:
-#     # '20170413_0510'
-#     if date_str in href.text:
-#         station_url = href.text
-#         print(station_url)
-#         break
-#     else:
-#         station_url = ''
-
-# url3 = url2 + station_url
-# r = requests.get(url3, allow_redirects=True)
-# with open(station_url, 'wb') as f:
-#     f.write(r.content)
