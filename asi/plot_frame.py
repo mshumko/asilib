Finished editing the plot_frame() function. I fixed the 'log' or 'lin' color normalization kwarg in plt, and now the plt.imshow object is returned.

asi/plot_frame.py
@@ -15,8 +15,8 @@ import asi.config as config
 
 def plot_frame(time: Union[datetime, str], mission: str, station: str, 
             force_download: bool=False, time_thresh_s: float=3, 
-            ax: plt.subplot=None, add_label: bool=True, color_map: str='gnuplot2',
-            color_bounds: Union[List[float], None]=None, log_norm: bool=True) -> plt.subplot:
+            ax: plt.subplot=None, add_label: bool=True, color_map: str='hot',
+            color_bounds: Union[List[float], None]=None, color_norm: str='log') -> plt.subplot:
     """
     Plots one ASI image frame given the mission (THEMIS or REGO), station, and 
     the day date-time parameters. If a file does not locally exist, the load()
@@ -49,17 +49,22 @@ def plot_frame(time: Union[datetime, str], mission: str, station: str,
         The lower and upper values of the color scale. If None, will 
         automatically set it to low=1st_quartile and 
         high=min(3rd_quartile, 10*1st_quartile)
-    log_norm: bool
-        Flag to set the logarithmic or linear color normalization.
+    color_norm: str
+        Sets the 'lin' linear or 'log' logarithmic color normalization.
 
     Returns
     -------
     ax: plt.subplot
         Same subplot object if ax was passed as an argument, or a new
         subplot object contaning the frame.
-    
+    im: plt.imshow
+        The plt.imshow object that can be used to add a colorbar.
+
     Example
     -------
+    ax, im = plot_frame(datetime(2016, 10, 29, 4, 15), 'REGO', 'GILL', color_norm='log')
+    plt.colorbar(im)
+    plt.show()
     """
     if ax is None:
         _, ax = plt.subplots()
@@ -67,19 +72,22 @@ def plot_frame(time: Union[datetime, str], mission: str, station: str,
     frame_time, frame = get_frame(time, mission, station, 
         force_download=force_download, time_thresh_s=time_thresh_s)
     
+    # Figure out the color_bounds from the frame data.
     if color_bounds is None:
-        # Figure out the color_bounds from the frame data.
-        q1, q3 = np.quantile(frame, (0.25, 0.75))
-        color_bounds = [q1, np.min([q3, 10*q1])]
-    if log_norm:
-        norm=colors.LogNorm(vmin=color_bounds[0], vmax=color_bounds[1])
-    else:
-        norm=colors.NoNorm(vmin=color_bounds[0], vmax=color_bounds[1])
+        lower, upper = np.quantile(frame, (0.25, 0.98))
+        color_bounds = [lower, np.min([upper, lower*10])]
 
-    ax.imshow(frame, cmap=color_map, norm=norm)
+    if color_norm == 'log':
+        norm=colors.LogNorm(vmin=color_bounds[0], vmax=color_bounds[1])
+    elif color_norm == 'lin':
+        norm=colors.Normalize(vmin=color_bounds[0], vmax=color_bounds[1])
+    else:
+        raise ValueError('color_norm must be either "log" or "lin".')
+
+    im = ax.imshow(frame, cmap=color_map, norm=norm)
     ax.text(0, 0, f"{mission}/{station}\n{frame_time.strftime('%Y-%m-%d %H:%M:%S')}", 
             va='bottom', transform=ax.transAxes, color='white')
-    return ax
+    return ax, im
     
 
 def get_frame(time: Union[datetime, str], mission: str, station: str, 
@@ -268,5 +276,7 @@ if __name__ == '__main__':
     # rego_data = load(datetime(2016, 10, 29, 4), 'REGO', 'GILL')
     # time_range: List[datetime] = [datetime(2016, 10, 29, 4, 15), datetime(2016, 10, 29, 4, 20)]
     # times, frames = get_frames(time_range, 'REGO', 'GILL')
-    ax = plot_frame(datetime(2016, 10, 29, 4, 15), 'REGO', 'GILL')
+    ax, im = plot_frame(datetime(2015, 4, 9, 7, 35, 6), 'REGO', 'FSMI', color_norm='log')
+    plt.colorbar(im)
+    plt.axis('off')
     plt.show()