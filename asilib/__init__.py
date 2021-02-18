21ac415f36fadfe7a96220af2993ef73ae715f55
Mykhaylo Shumko <msshumko@gmail.com>
2021-02-18 13:04:51 -0700

    Bump version: 0.1.3 → 0.1.4

--- asilib/__init__.py
+++ asilib/__init__.py
@@ -1,7 +1,7 @@
 import warnings
 import pathlib
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 # Check that the package has been configured and config.py file exists.
 here = pathlib.Path(__file__).parent.resolve()
