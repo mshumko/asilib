7bd3ce9fc90015bd353456ce2d2edf7d6e277b1c
Mykhaylo <msshumko@gmail.com>
2021-04-25 12:56:01 -0600

    I updated the Sphinx contributing and the conf.py docs configuration settings.

--- docs/conf.py
+++ docs/conf.py
@@ -48,6 +48,15 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '../asilib/tests']
 #
 html_theme = 'sphinx_rtd_theme'
 
+html_theme_options = {
+    # Toc options
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': True
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
