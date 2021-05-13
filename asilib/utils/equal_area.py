3fa31a8439d464bfd593b0f25729364ec1700dd8
Mykhaylo <msshumko@gmail.com>
2021-05-13 13:30:36 -0400

    Working on the equal areas code. I think I have a unit issue.

--- asilib/utils/equal_area.py
+++ asilib/utils/equal_area.py
@@ -1,10 +1,11 @@
 import numpy as np
+import pandas as pd
 
 import asilib
 
 earth_radius_km = 6371 # Earth radius
 
-def equal_area(mission, station, lla, box_km=(10, 10)):
+def equal_area(mission, station, lla, box_km=(5, 5), alt_thresh_km=3):
     """
     Given a square are in kilometers and a series of (latitude, 
     longitude, altitude) coordinates, calculate the pixel box
@@ -26,7 +27,7 @@ def equal_area(mission, station, lla, box_km=(10, 10)):
 
     Returns
     -------
-    box_pixels: np.ndarray
+    pixel_mask: np.ndarray
         An array with (n_time, 2) dimensions with the columns
         represneting the latitude and longitude coordinates, in 
         that order.
@@ -35,10 +36,45 @@ def equal_area(mission, station, lla, box_km=(10, 10)):
 
     cal_dict = asilib.load_cal_file(mission, station)
 
+    # Get numpy array if pd.DataFrame passed
+    if isinstance(lla, pd.DataFrame):
+        lla = lla.to_numpy()
+
+    initial_shape = lla.shape
+
+    # Check that the altitudes are in the appropriate range
+    if len(initial_shape) == 1:  # 1d array
+        assert np.min(np.abs(cal_dict['FULL_MAP_ALTITUDE']/1000-lla[-1])) < alt_thresh_km , (
+            f'Got {lla[-1]} km altitude, but it must be one of these: {cal_dict["FULL_MAP_ALTITUDE"]/1000}')
+        alt_index = np.argmin(np.abs(cal_dict['FULL_MAP_ALTITUDE']/1000-lla[-1]))
+        lla = np.array([lla])
+    elif len(initial_shape) == 2:  # 2d array
+        for alt in lla[:, -1]:
+            assert np.min(np.abs(cal_dict['FULL_MAP_ALTITUDE']/1000-alt)) < alt_thresh_km , (
+                f'Got {alt} km altitude, but it must be one of these: {cal_dict["FULL_MAP_ALTITUDE"]/1000}')
+        alt_index = np.argmin(np.abs(cal_dict['FULL_MAP_ALTITUDE']/1000-alt))
+
+    pixel_mask = np.zeros((lla.shape[0], *cal_dict['FULL_MAP_LATITUDE'].shape[1:]))
+
     dlat = _dlat(box_km[1], lla[:, -1])
     dlon = _dlon(box_km[0], lla[:, -1], lla[:, 0])
 
-    return
+    lat_map = cal_dict['FULL_MAP_LATITUDE'][alt_index, :, :]
+    lon_map = cal_dict['FULL_MAP_LONGITUDE'][alt_index, :, :]
+
+    for i, (lat, lon, _)in enumerate(lla):
+        idx_box = np.where(
+            (lat_map >= lat-dlat[i]/2) &
+            (lat_map <= lat+dlat[i]/2) &
+            (lon_map >= lon-dlon[i]/2) &
+            (lon_map <= lon+dlon[i]/2)
+        )
+        pixel_mask[i, idx_box] = 1
+
+    if len(initial_shape) == 1:
+        return pixel_mask.reshape(cal_dict['FULL_MAP_LATITUDE'].shape[1:])
+    else:
+        return pixel_mask
 
 
 def _dlat(d, alt):
@@ -58,7 +94,7 @@ def _dlat(d, alt):
     dlat: float or np.ndarray
         A float or an array of the corresponding latitude differences in degrees.
     """
-    return np.deg2rad(np.divide(d, (earth_radius_km+alt)))
+    return np.rad2deg(np.divide(d, (earth_radius_km+alt)))
 
 
 def _dlon(d, alt, lat):
