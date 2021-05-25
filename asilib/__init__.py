452426808dafffe91e8a797e3601f5870373b8bc
Mykhaylo Shumko <msshumko@gmail.com>
2021-05-25 14:01:30 -0400

    Bump version: 0.2.3 → 0.3.0

--- asilib/__init__.py
+++ asilib/__init__.py
@@ -3,7 +3,7 @@ import pathlib
 import importlib.util
 import configparser
 
-__version__ = '0.2.3'
+__version__ = '0.3.0'
 
 # Load the configuration settings.
 HERE = pathlib.Path(__file__).parent.resolve()
