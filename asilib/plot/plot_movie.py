3b4d56ed83097f00a8a3b6920f732a64eb297993
Mykhaylo Shumko <msshumko@gmail.com>
2021-05-26 13:30:24 -0400

    Fixed a bug in make_movie_generator().

--- asilib/plot/plot_movie.py
+++ asilib/plot/plot_movie.py
@@ -237,7 +237,7 @@ def plot_movie_generator(
     # will halt here. This way the errors due to missing data will be raised up front.
     user_input = yield
     # user_input can be used to get the frame_times and frames out of the generator.
-    if 'data' in user_input.lower():
+    if isinstance(user_input, str) and 'data' in user_input.lower():
         yield Frames(frame_times, frames)
 
     for frame_time, frame in zip(frame_times, frames):
