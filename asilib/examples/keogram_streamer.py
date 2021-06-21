b99d9c3512d0756c69f15acd36ec73deeae2ea37
Mykhaylo Shumko <mykhaylo.shumko@nasa.gov>
2021-06-21 18:53:32 -0500

    Added a script to make a THEMIS keogram of a streamer.

---
+++ asilib/examples/keogram_streamer.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+import asilib
+
+mission='THEMIS'
+station='RANK'
+
+asilib.plot_keogram(['2008-02-05/08:00:00', '2008-02-05/09:00:00'], mission, station, map_alt=110)
+plt.show()
