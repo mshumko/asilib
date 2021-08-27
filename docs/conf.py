15f80326558c8fb4a1796b82df514460a668e7b1
Mykhaylo Shumko <mykhaylo.shumko@nasa.gov>
2021-08-27 16:51:11 -0400

    Added a mock import to Sphinx to ignore the cartopy package.

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,8 @@ extensions = [
     'nbsphinx'
 ]
 
+autodoc_mock_imports = ["cartopy"]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
