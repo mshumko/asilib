e6147ff1a3309c602c1aa48711ebc8a90a7863e1
Mykhaylo Shumko <mykhaylo.shumko@nasa.gov>
2021-10-05 18:20:06 -0400

    Bump version: 0.7.4 → 0.7.5

--- asilib/__init__.py
+++ asilib/__init__.py
@@ -2,7 +2,7 @@ import warnings
 import pathlib
 import configparser
 
-__version__ = '0.7.4'
+__version__ = '0.7.5'
 
 # Load the configuration settings.
 HERE = pathlib.Path(__file__).parent.resolve()
