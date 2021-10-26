8c0b14c833a7686f5a8261ec38b0d22484413a5f
Mykhaylo Shumko <msshumko@gmail.com>
2021-10-26 15:49:53 -0400

    Updated the tests.

--- asilib/tests/test_download_themis.py
+++ asilib/tests/test_download_themis.py
@@ -17,6 +17,8 @@ class TestDownloadThemis(unittest.TestCase):
         """Set up a few variables."""
         self.day = datetime(2016, 10, 29, 4)
         self.location_code = 'Gill'
+        self.asi_array_code = 'THEMIs'
+
         self.url = (
             download_themis.IMG_BASE_URL
             + f'{self.location_code.lower()}/{self.day.year}/{str(self.day.month).zfill(2)}/'
@@ -33,7 +35,7 @@ class TestDownloadThemis(unittest.TestCase):
         self.assertNotEqual(status_code // 100, 5)
         return
 
-    def test_download_themis_img_one_file(self):
+    def test_download_img(self):
         """
         Test the full THEMIS data downloader and download an hour file
         clg_l1_rgf_luck_2020080104_v01.cdf to ./themis/.
@@ -41,9 +43,11 @@ class TestDownloadThemis(unittest.TestCase):
         temp_image_dir = pathlib.Path(asilib.config['ASI_DATA_DIR'], 'themis')
         temp_image_path = temp_image_dir / 'thg_l1_asf_gill_2016102904_v01.cdf'
 
-        download_themis.download_themis_img(self.location_code, self.day, force_download=True)
+        save_path = asilib.download_image(self.asi_array_code, self.location_code, time=self.day, 
+            force_download=True)
 
-        self.assertTrue(temp_image_path.is_file())
+        assert temp_image_path == save_path[0]
+        assert temp_image_path.is_file()
         return
 
 
