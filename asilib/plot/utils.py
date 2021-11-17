4633a15945cf88b02e4d10b6ce9de4c9dd88cfc4
Mykhaylo Shumko <msshumko@gmail.com>
2021-11-17 14:50:47 -0500

    Updated the utility function doc strings.

--- asilib/plot/utils.py
+++ asilib/plot/utils.py
@@ -3,11 +3,18 @@ import matplotlib.colors as colors
 
 
 def get_color_bounds(image):
+    """
+    A decent default for the minimum and maximum colorbar values for aurora images. This way
+    bright objects like the moon don't saturate the image while preserving enough dynamic range.
+    """
     lower, upper = np.nanquantile(image, (0.25, 0.98))
     color_bounds = [lower, np.min([upper, lower * 10])]
     return color_bounds
 
 def get_color_map(asi_array_code, color_map):
+    """
+    Color maps for the THEMIS and REGO ASIs. 
+    """
     if (color_map == 'auto') and (asi_array_code.lower() == 'themis'):
         color_map = 'Greys_r'
     elif (color_map == 'auto') and (asi_array_code.lower() == 'rego'):
@@ -17,6 +24,10 @@ def get_color_map(asi_array_code, color_map):
     return color_map
 
 def get_color_norm(color_norm, color_bounds):
+    """
+    Sets the normalization of the images to linear or logarithmic. You will typically call
+    get_color_bounds() to get the color_bounds for this function.
+    """
     if color_norm == 'log':
         norm = colors.LogNorm(vmin=color_bounds[0], vmax=color_bounds[1])
     elif color_norm == 'lin':
