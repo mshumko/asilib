730617f4e1bba06e9b729c5e1cb8dec1bda31b00
Mykhaylo Shumko <mykhaylo.shumko@nasa.gov>
2022-01-05 20:28:52 -0500

    Added a NO Quality Assurance comment to make flake8 gnore the p.remove() line.

--- asilib/plot/animate_map.py
+++ asilib/plot/animate_map.py
@@ -276,7 +276,7 @@ def animate_map_generator(
 
     for image_time, image in zip(image_times, images):
         if 'p' in locals():
-            p.remove()
+            p.remove() # noqa
 
         # if-else statement is to recalculate color_bounds for every image
         # and set it to _color_bounds. If _color_bounds did not exist,
