a588899c334613fe8ea7a873c22a4c203dcb0386
Mykhaylo Shumko <msshumko@gmail.com>
2023-02-22 11:06:41 -0500

    Reorganizing the mapping functions by making a general map() function that automatically switches between cartopy or asilib's map libraries, a cartopy_map() function that makes a cartopy map, and a simple_map() function that uses asilib's map engine.
    A hurdle to figure out is how to handle the plot creating
    parameters in an intuitive way.

--- asilib/map.py
+++ asilib/map.py
@@ -8,70 +8,103 @@ import zipfile
 import shapefile
 import matplotlib.pyplot as plt
 import numpy as np
+import numpy.ma
 
-try:  # To automatically switch between cartopy and asilib maps
+try:
     import cartopy.crs as ccrs
     import cartopy.feature as cfeature
     cartopy_imported = True
-except ImportError:
+except ImportError as err:
+    # You can also get a ModuleNotFoundError if cartopy is not installed
+    # (as compared to failed to import), but it is a subclass of ImportError.
     cartopy_imported =  False
 
 import asilib
 
 
-def make_map(map_engine='auto'):
-    # Call the two functions here
+def map(
+    lon_bounds: tuple = (-160, -50),
+    lat_bounds: tuple = (40, 82),
+    ax: plt.Axes = None,
+    coast_color: str = 'k',
+    land_color: str = 'g',
+    ocean_color: str = 'w',
+    ):
+    """
+    Create a geographic map using cartopy (if installed) or asilib's own 
+    map library.
 
-    map_engine = map_engine.lower()
-    assert map_engine in ['auto', 'cartopy', 'asilib']
-    if (map_engine == 'auto') and (cartopy_imported):
+    Parameters
+    ----------
+    lon_bounds: tuple
+        The map's longitude boundary.
+    lat_bounds: tuple
+        The map's latitude boundary.
+    ax: plt.Axes
+        The subplot to put the map on.
+    coast_color: str
+        The coast color.
+    land_color: str
+        The land color.
+    ocean_color: str
+        The ocean color.
+
+    Returns
+    -------
+    plt.Axes
+        The subplot object containing the map.
+
+    Example
+    -------
+
+    See Also
+    --------
+    asilib.make_simple_map: 
+        Make a simple map using asilib's own map library.
+    asilib.make_cartopy_map:
+        Make a map using the cartopy library.
+    """
+    if cartopy_imported:
         # use cartopy
-        pass
-    elif (map_engine == 'auto') and (not cartopy_imported):
-        # asilib engine
-        pass
-    elif (map_engine == 'cartopy'):
-        # cartopy engine
-        if not cartopy_imported:
-            raise ImportError("cartopy can't be imported.")
         pass
     else:
         # asilib engine
         pass
     return
 
-def make_map(
-    file: Union[str, pathlib.Path] = 'ne_10m_land',
+def simple_map(
+    lon_bounds: tuple = (-140, -60),
+    lat_bounds: tuple = (40, 82),
+    ax: plt.Axes = None,
     coast_color: str = 'k',
     land_color: str = 'g',
     ocean_color: str = 'w',
-    ax: plt.Axes = None,
-    lon_bounds: tuple = (-140, -60),
-    lat_bounds: tuple = (40, 82),
+    file: Union[str, pathlib.Path] = 'ne_10m_land',
 ) -> plt.Axes:
     """
-    Makes a map using the mercator projection with a shapefile read in by the pyshp package.
+    Makes a simple map using the mercator projection with a shapefile read in by the pyshp package.
 
     A good place to download shapefiles is
     https://www.naturalearthdata.com/downloads/10m-physical-vectors/.
 
     Parameters
     ----------
-    file: str or pathlib.Path
-        The path to the shapefile zip archive. If str, it will try to load the
-        shapefile in asilib/data/{file}.
+    lon_bounds: tuple
+        The map's longitude bounds.
+    lat_bounds: tuple
+        The map's latitude bounds.
+    ax: plt.Axes
+        The subplot to put the map on.
     coast_color: str
         The coast color. If None will not draw it.
     land_color: str
         The land color. If None will not draw it.
     ocean_color: str
         The ocean color. If None will not draw it.
-    ax: plt.Axes
-        The subplot to put the map on.
-    lon_bounds: tuple
-        The map's longitude bounds.
-    lat_bounds: tuple
-        The map's latitude bounds.
+
+    file: str or pathlib.Path
+        The path to the shapefile zip archive. If str, it will try to load the
+        shapefile in asilib/data/{file}.
 
     Returns
     -------
@@ -98,42 +131,41 @@ def make_map(
     # matplotlib draws straight (annoying) lines between them. This code uses
     # the jumps bool array and masked_arrays to remove those lines.
     jumps = (np.abs(lats[1:] - lats[:-1]) > 3) | (np.abs(lons[1:] - lons[:-1]) > 3)
-    mlats = ma.masked_array(lats[:-1], mask=jumps)
-    mlons = ma.masked_array(lons[:-1], mask=jumps)
+    mlats = numpy.ma.masked_array(lats[:-1], mask=jumps)
+    mlons = numpy.ma.masked_array(lons[:-1], mask=jumps)
 
     split_lats = _consecutive(lats, jumps)
     split_lons = _consecutive(lons, jumps)
 
     if ax is None:
         _, ax = plt.subplots()
+
     if ocean_color is not None:
         ax.set_facecolor(ocean_color)
-        pass
     if coast_color is not None:
         ax.plot(mlats, mlons, coast_color)
     if land_color is not None:
         for split_lon, split_lat in zip(split_lons, split_lats):
             ax.fill(split_lat, split_lon, land_color, zorder=0)
 
-    # ax.set_aspect('equal', adjustable='box')
-
     ax.set_xlim(lon_bounds)
     ax.set_ylim(lat_bounds)
     return ax
 
-def create_cartopy_map(
-    map_style: str = 'green',
+def cartopy_map(
     lon_bounds: tuple = (-160, -50),
     lat_bounds: tuple = (40, 82),
     fig_ax: dict = None,
+    coast_color: str = 'k',
+    land_color: str = 'g',
+    ocean_color: str = 'w',
 ) -> plt.Axes:
     """
     A helper function to create two map styles: a simple black and white map, and
     a more sophisticated map with green land.
+
     Parameters
     ----------
-    map_style: str
-        The map color style, can be either 'green' or 'white'.
     lon_bounds: tuple or list
         A tuple of length 2 specifying the map's longitude bounds.
     lat_bounds: tuple or list
@@ -141,11 +173,29 @@ def create_cartopy_map(
     fig_ax: dict
         Make a map on an existing figure. The dictionary key:values must be
         'fig': figure object, and 'ax': the subplot position in the
-        (nrows, ncols, index) format, or a GridSpec object.
+        (nrows, ncols, index) format, or a GridSpec object. For example
+        fig_ax = {}
+    coast_color: str
+        The coast color. If None will not draw it.
+    land_color: str
+        The land color. If None will not draw it.
+    ocean_color: str
+        The ocean color. If None will not draw it.
+
     Returns
     -------
     ax: plt.Axes
         The subplot object with a cartopy map.
+    
+    Example
+    -------
+    TODO: Complete example.
+    import asilib
+    import matplotlib.pyplot as plt
+
+    fig = plt.figure()
+    ax = asilib.cartopy_map(fig_ax={fig:})
+
     Raises
     ------
     ValueError:
@@ -167,18 +217,14 @@ def create_cartopy_map(
             # If fig_ax['ax'] is in the format XYZ or a gridspec object.
             ax = fig_ax['fig'].add_subplot(fig_ax['ax'], projection=projection)
 
+    if land_color is not None:
+        ax.add_feature(cfeature.LAND, color=land_color)
+    if ocean_color is not None:
+        ax.add_feature(cfeature.OCEAN, color=ocean_color)
+    if coast_color is not None:
+        ax.add_feature(cfeature.COASTLINE, color=coast_color)
+    ax.gridlines(linestyle=':')
     ax.set_extent(plot_extent, crs=ccrs.PlateCarree())
-
-    if map_style == 'green':
-        ax.add_feature(cfeature.LAND, color='green')
-        ax.add_feature(cfeature.OCEAN)
-        ax.add_feature(cfeature.COASTLINE)
-        ax.gridlines(linestyle=':')
-    elif map_style == 'white':
-        ax.coastlines()
-        ax.gridlines(linestyle=':')
-    else:
-        raise ValueError("Only the 'white' and 'green' map_style are implemented.")
     return ax
 
 
