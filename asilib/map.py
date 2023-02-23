9a641b63c549d3806005506ad269a422466d6c7d
Mykhaylo Shumko <msshumko@gmail.com>
2023-02-23 09:54:05 -0500

    Updating geographic map documentation.

--- asilib/map.py
+++ asilib/map.py
@@ -3,15 +3,28 @@ Plot geographic maps using cartopy or the simple built-in function. Before you
 project an ASI image onto a map, you will need to create a map using the 
 following functions.
 
-The simplest way to make a map is via :py:meth:`~asilib.map.create_map` that
-by default makes a map above North America. :py:meth:`~asilib.map.create_map` 
-automatically chooses what library to plot the map: cartopy if it is installed,
-or asilib's :py:meth:`~asilib.map.create_simple_map` function to create a simple
-map.
+The simplest way to create a map is via :py:meth:`~asilib.map.create_map` that
+by default creates a map above North America. :py:meth:`~asilib.map.create_map` 
+is a wrapper that automatically chooses what library to plot the map: cartopy 
+if it is installed, or asilib's :py:meth:`~asilib.map.create_simple_map` 
+function to create a simple map if cartopy is not installed. All of these functions
+output the subplot object with the map.
 
 You can override this automatic behavior by calling the underlying functions directly:
 :py:meth:`~asilib.map.create_simple_map` or :py:meth:`~asilib.map.create_cartopy_map`.
+
+The two functions are called similarly with the exception of the ``ax``  kwarg if you 
+need to specify what subplot to create the map on:
+
+- for :py:meth:`~asilib.map.create_simple_map` you must pass in a ``ax`` subplot object, and 
+- for :py:meth:`~asilib.map.create_cartopy_map`, you need to pass in a ``fig_ax`` tuple containing two elements. The first element is the ```plt.Figure`` object, and the second element is a 3 digit number (or a tuple) specifying where to place that subplot. For example,
+
+    .. code-block:: python
+
+        fig = plt.Figure()
+        ax = asilib.map.create_cartopy_map(fig_ax=(fig, 111))
 """
+
 import pathlib
 from typing import List, Union
 import zipfile
@@ -36,7 +49,7 @@ import asilib
 def create_map(
     lon_bounds: tuple = (-160, -50),
     lat_bounds: tuple = (40, 82),
-    ax: Union[plt.Axes, dict] = None,
+    ax: Union[plt.Axes, tuple] = None,
     coast_color: str = 'k',
     land_color: str = 'g',
     ocean_color: str = 'w',
@@ -51,9 +64,11 @@ def create_map(
         The map's longitude bounds.
     lat_bounds: tuple
         The map's latitude bounds.
-    ax: plt.Axes, dict
+    ax: plt.Axes, tuple
         The subplot to put the map on. If cartopy is installed, ```ax``` must be
-        a dictionary specifying the figure object and subplot position
+        a two element tuple specifying the ``plt.Figure`` object and subplot position
+        passed directly as ``args`` into  
+        `fig.add_subplot() <https://matplotlib.org/stable/api/figure_api.html#matplotlib.figure.Figure.add_subplot>`_.
     coast_color: str
         The coast color. If None will not draw it.
     land_color: str
@@ -74,9 +89,9 @@ def create_map(
     See Also
     --------
     :py:meth:`~asilib.map.create_simple_map`: 
-        Make a simple map using asilib's own map library.
+        Create a simple map using asilib's own map library.
     :py:meth:`~asilib.map.create_cartopy_map`:
-        Make a map using the cartopy library.
+        Create a map using the cartopy library.
     """
     if cartopy_imported:
         if ax is not None:
@@ -109,10 +124,7 @@ def create_simple_map(
     file: Union[str, pathlib.Path] = 'ne_10m_land',
 ) -> plt.Axes:
     """
-    Makes a simple map using the mercator projection with a shapefile read in by the pyshp package.
-
-    A good place to download shapefiles is
-    https://www.naturalearthdata.com/downloads/10m-physical-vectors/.
+    Create a simple map without cartopy.
 
     Parameters
     ----------
@@ -128,10 +140,10 @@ def create_simple_map(
         The land color. If None will not draw it.
     ocean_color: str
         The ocean color. If None will not draw it.
-
     file: str or pathlib.Path
         The path to the shapefile zip archive. If str, it will try to load the
-        shapefile in asilib/data/{file}.
+        shapefile in asilib/data/{file}. You can download other shapefiles from
+        https://www.naturalearthdata.com/downloads/10m-physical-vectors/.
 
     Returns
     -------
@@ -225,10 +237,16 @@ def create_cartopy_map(
     lat_bounds: tuple or list
         A tuple of length 2 specifying the map's latitude bounds.
     fig_ax: tuple
-        Add a map on an existing subplot. The two-element tuple must have the
-        plt.figure object and the location of the subplot in the "Three integers 
-        (nrows, ncols, index)" format defined by plt.subplot(). See
-        https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.subplot.html.
+        A two element tuple specifying the ``plt.Figure`` object and subplot position
+        passed directly as ``args`` into  
+        `fig.add_subplot() <https://matplotlib.org/stable/api/figure_api.html#matplotlib.figure.Figure.add_subplot>`_.
+        For example:
+
+        .. code-block:: python
+
+            fig = plt.Figure()
+            ax = asilib.map.create_cartopy_map(fig_ax=(fig, 111))
+        
     coast_color: str
         The coast color. If None will not draw it.
     land_color: str
@@ -243,39 +261,38 @@ def create_cartopy_map(
     
     Examples
     --------
-    >>> import asilib.map
-    >>> import numpy as np
-    >>> import matplotlib.pyplot as plt
+    import asilib.map
+    import numpy as np
+    import matplotlib.pyplot as plt
 
     # Create a cartopy map above Scandinavia in a single plot
 
-    >>> ax = asilib.map.create_cartopy_map(lon_bounds=[0, 38], lat_bounds=[50, 75])
-    >>> ax.set_title('Generated via asilib.map.create_cartopy_map()')
+    ax = asilib.map.create_cartopy_map(lon_bounds=[0, 38], lat_bounds=[50, 75])
+    ax.set_title('Generated via asilib.map.create_cartopy_map()')
     
     # If you have cartopy installed, the following example creates the same plot.
 
-    >>> bx = asilib.map.create_map(lon_bounds=[0, 38], lat_bounds=[50, 75])
-    >>> bx.set_title('Generated via asilib.map.create_map()')
+    bx = asilib.map.create_map(lon_bounds=[0, 38], lat_bounds=[50, 75])
+    bx.set_title('Generated via asilib.map.create_map()')
 
-    # The above examples made a map on one subplot. But what if you have multiple 
-    # subplots?
+    # The above examples made a map on one subplot. But what if you have multiple subplots?
 
-    >>> fig = plt.figure(figsize=(6, 10))
-    >>> cx = asilib.map.create_map(lon_bounds=[0, 38], lat_bounds=[50, 75], ax=(fig, (2,1,1)))
-    >>> dx = fig.add_subplot(2,1,2)
-    >>> dx.plot(np.arange(10), np.random.rand(10))
-    >>> fig.suptitle('Two subplots with equal aspect ratios')
+    fig = plt.figure(figsize=(6, 10))
+    cx = asilib.map.create_map(lon_bounds=[0, 38], lat_bounds=[50, 75], ax=(fig, (2,1,1)))
+    dx = fig.add_subplot(2,1,2)
+    dx.plot(np.arange(10), np.random.rand(10))
+    fig.suptitle('Two subplots with equal aspect ratios')
 
     # Another multi-subplot example with different height ratios. The syntax is the same as
     # in plt.subplot() (See the args section in 
     # https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.subplot.html).
 
-    >>> fig = plt.figure(figsize=(6, 10))
-    >>> cx = asilib.map.create_map(lon_bounds=[0, 38], lat_bounds=[50, 75], ax=(fig, (3,1,(1,2))))
-    >>> dx = fig.add_subplot(3,1,3)
-    >>> dx.plot(np.arange(10), np.random.rand(10))
-    >>> fig.suptitle('Two subplots with differing aspect ratios')
-    >>> plt.show()
+    fig = plt.figure(figsize=(6, 10))
+    cx = asilib.map.create_map(lon_bounds=[0, 38], lat_bounds=[50, 75], ax=(fig, (3,1,(1,2))))
+    dx = fig.add_subplot(3,1,3)
+    dx.plot(np.arange(10), np.random.rand(10))
+    fig.suptitle('Two subplots with differing aspect ratios')
+    plt.show()
     """
     plot_extent = [*lon_bounds, *lat_bounds]
     central_lon = np.mean(lon_bounds)
@@ -319,8 +336,7 @@ if __name__ == '__main__':
     ax = asilib.map.create_simple_map(lon_bounds=[0, 38], lat_bounds=[50, 75])
     ax.set_title('Generated via asilib.map.create_simple_map()')
 
-    # The above examples made a map on one subplot. But what if you have multiple 
-    # subplots?
+    # The above examples made a map on one subplot. But what if you have multiple subplots?
 
     fig = plt.figure(figsize=(6, 10))
     bx = fig.add_subplot(2,1,1)
