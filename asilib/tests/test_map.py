3a39b7db66f43cedb0ba153368fe79cbd3c2b9d9
Mykhaylo Shumko <msshumko@gmail.com>
2023-03-31 08:54:13 -0400

    Added tests for cartopy and simple maps. The cartopy maps will only get tested if cartopy is importable.

--- asilib/tests/test_map.py
+++ asilib/tests/test_map.py
@@ -1,15 +1,25 @@
 """
-Test the geographic map creation functions. This does not test the cartopy 
-maps.
+Test the simple and cartopy geographic map creation functions.
 """
 
+import pytest
 import matplotlib.testing.decorators
 
+try:
+    import cartopy
+
+    cartopy_imported = True
+except ImportError as err:
+    # You can also get a ModuleNotFoundError if cartopy is not installed
+    # (as compared to failed to import), but it is a subclass of ImportError.
+    cartopy_imported = False
+
+## SIMPLE MAPS ##
 
 @matplotlib.testing.decorators.image_comparison(
-    baseline_images=['test_create_map_no_subplot'], tol=10, remove_text=True, extensions=['png']
+    baseline_images=['test_create_simple_map_no_subplot'], tol=10, remove_text=True, extensions=['png']
 )
-def test_create_map_no_subplot():
+def test_create_simple_map_no_subplot():
     """
     Create a geographic map on one subplot.
     """
@@ -22,9 +32,9 @@ def test_create_map_no_subplot():
 
 
 @matplotlib.testing.decorators.image_comparison(
-    baseline_images=['test_create_map_equal_subplots'], tol=10, remove_text=True, extensions=['png']
+    baseline_images=['test_create_simple_map_equal_subplots'], tol=10, remove_text=True, extensions=['png']
 )
-def test_create_map_equal_subplots():
+def test_create_simple_map_equal_subplots():
     """
     Create a geographic map on one subplot and random data on the second. The subplots
     have have unequal sizes.
@@ -42,12 +52,12 @@ def test_create_map_equal_subplots():
 
 
 @matplotlib.testing.decorators.image_comparison(
-    baseline_images=['test_create_map_unequal_subplots'],
+    baseline_images=['test_create_simple_map_unequal_subplots'],
     tol=10,
     remove_text=True,
     extensions=['png'],
 )
-def test_create_map_unequal_subplots():
+def test_create_simple_map_unequal_subplots():
     """
     Create a geographic map on one subplot and random data on the second. The subplots
     have have unequal sizes.
@@ -66,9 +76,9 @@ def test_create_map_unequal_subplots():
 
 
 @matplotlib.testing.decorators.image_comparison(
-    baseline_images=['test_create_map_gridspec'], tol=10, remove_text=True, extensions=['png']
+    baseline_images=['test_create_simple_map_gridspec'], tol=10, remove_text=True, extensions=['png']
 )
-def test_create_map_gridspec():
+def test_create_simple_map_gridspec():
     """
     Create a geographic map in a subplot created using gridspec.
     """
@@ -79,5 +89,84 @@ def test_create_map_gridspec():
     fig = plt.figure()
     gs = matplotlib.gridspec.GridSpec(1, 1, fig)
     dx = asilib.map.create_simple_map(lon_bounds=[0, 38], lat_bounds=[50, 75], fig_ax=(fig, gs))
+    dx.set_title('Map made using gridspec')
+    return
+
+
+## CARTOPY MAPS ##
+@pytest.mark.skipif(not cartopy_imported, reason='cartopy not installed')
+@matplotlib.testing.decorators.image_comparison(
+    baseline_images=['test_create_cartopy_map_no_subplot'], tol=10, remove_text=True, extensions=['png']
+)
+def test_create_cartopy_map_no_subplot():
+    """
+    Create a geographic map on one subplot.
+    """
+    import asilib.map
+    import matplotlib.pyplot as plt
+
+    ax = asilib.map.create_cartopy_map(lon_bounds=[0, 38], lat_bounds=[50, 75])
+    ax.set_title('Generated via asilib.map.create_cartopy_map()')
+    return
+
+@pytest.mark.skipif(not cartopy_imported, reason='cartopy not installed')
+@matplotlib.testing.decorators.image_comparison(
+    baseline_images=['test_create_cartopy_map_equal_subplots'], tol=10, remove_text=True, extensions=['png']
+)
+def test_create_cartopy_map_equal_subplots():
+    """
+    Create a geographic map on one subplot and random data on the second. The subplots
+    have have unequal sizes.
+    """
+    import asilib.map
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    fig = plt.figure(figsize=(6, 10))
+    bx = asilib.map.create_cartopy_map(lon_bounds=[0, 38], lat_bounds=[50, 75], fig_ax=(fig, 211))
+    cx = fig.add_subplot(2, 1, 2)
+    cx.plot(np.arange(10), np.arange(10))
+    fig.suptitle('Two subplots with equal sizes')
+    return
+
+@pytest.mark.skipif(not cartopy_imported, reason='cartopy not installed')
+@matplotlib.testing.decorators.image_comparison(
+    baseline_images=['test_create_cartopy_map_unequal_subplots'],
+    tol=10,
+    remove_text=True,
+    extensions=['png'],
+)
+def test_create_cartopy_map_unequal_subplots():
+    """
+    Create a geographic map on one subplot and random data on the second. The subplots
+    have have unequal sizes.
+    """
+    import asilib.map
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    fig = plt.figure(figsize=(6, 10))
+    dx = (3, 1, (1, 2))
+    dx = asilib.map.create_cartopy_map(lon_bounds=[0, 38], lat_bounds=[50, 75], fig_ax=(fig, dx))
+    ex = fig.add_subplot(3, 1, 3)
+    ex.plot(np.arange(10), np.arange(10))
+    fig.suptitle('Two subplots with unequal sizes')
+    return
+
+@pytest.mark.skipif(not cartopy_imported, reason='cartopy not installed')
+@matplotlib.testing.decorators.image_comparison(
+    baseline_images=['test_create_cartopy_map_gridspec'], tol=10, remove_text=True, extensions=['png']
+)
+def test_create_cartopy_map_gridspec():
+    """
+    Create a geographic map in a subplot created using gridspec.
+    """
+    import asilib.map
+    import matplotlib.pyplot as plt
+    import matplotlib.gridspec
+
+    fig = plt.figure()
+    gs = matplotlib.gridspec.GridSpec(1, 1, fig)
+    dx = asilib.map.create_cartopy_map(lon_bounds=[0, 38], lat_bounds=[50, 75], fig_ax=(fig, gs))
     dx.set_title('Map made using gridspec')
     return
