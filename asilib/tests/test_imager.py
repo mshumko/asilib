a96c227b5016eb3868f0ec74e437689f120dfc69
Mykhaylo Shumko <msshumko@gmail.com>
2023-03-31 11:36:47 -0400

    Adding more tests.

--- asilib/tests/test_imager.py
+++ asilib/tests/test_imager.py
@@ -5,17 +5,10 @@ import matplotlib.testing.decorators
 
 from asilib.asi.fake_asi import fake_asi
 
-@matplotlib.testing.decorators.image_comparison(
-    baseline_images=['test_fisheye'], tol=10, remove_text=True, extensions=['png']
-)
-def test_fisheye():
-    """
-    Tests that the fake_asi produces the correct image.
-    """
-    asi = fake_asi('GILL', time='2015-01-01T15:14:00.17')
-    asi.plot_fisheye(color_bounds=(1, 255), origin=(0.85, 0.15), cardinal_directions='NEWS')
-    return
 
+##########################################
+########## TEST DATA ACCESS ##############
+##########################################
 def test_time():
     """
     Test if the fake_asi timestamp is correctly accessed.
@@ -45,4 +38,45 @@ def test_time_range():
         '20150101_180000_GILL_fake_asi.images',
         '20150101_190000_GILL_fake_asi.images'
         ]
+    return
+
+def test_iter_files():
+    """
+    Test Imager.iter_files()
+    """
+    asi = fake_asi('GILL', time_range=('2015-01-01T15:00:15.17', '2015-01-01T19:30'))
+    n_timestamps_reference = [358, 360, 360, 360, 181]
+
+    counter = 0
+    for (_times, _images), ref_n in zip(asi.iter_files(), n_timestamps_reference):
+        assert _times.shape[0] == ref_n
+        assert _images.shape[0] == ref_n
+        counter += 1
+
+    assert counter == len(n_timestamps_reference)
+    return
+
+##########################################
+############# TEST PLOTTING ##############
+##########################################
+@matplotlib.testing.decorators.image_comparison(
+    baseline_images=['test_fisheye'], tol=10, remove_text=True, extensions=['png']
+)
+def test_fisheye():
+    """
+    Tests that the fake_asi produces the correct image.
+    """
+    asi = fake_asi('GILL', time='2015-01-01T15:14:00.17')
+    asi.plot_fisheye(color_bounds=(1, 255), origin=(0.85, 0.15), cardinal_directions='NEWS')
+    return
+
+@matplotlib.testing.decorators.image_comparison(
+    baseline_images=['test_plot_keogram'], tol=10, remove_text=True, extensions=['png']
+)
+def test_plot_keogram():
+    """
+    Tests that the fake_asi produces the correct keogram.
+    """
+    asi = fake_asi('GILL', time_range=('2015-01-01T15:00:15.17', '2015-01-01T19:30'))
+    asi.plot_keogram()
     return
