c63e8bf2fd7afb84d57a3b2a0d4b8cab56ceb1d7
Mykhaylo <msshumko@gmail.com>
2023-04-19 18:10:24 -0400

    Updated map_arc example.

--- asilib/examples/map_arc.py
+++ asilib/examples/map_arc.py
@@ -10,6 +10,8 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 import asilib
+import asilib.map
+import asilib.asi.themis
 
 time = datetime(2007, 3, 13, 5, 8, 45)
 asi_array_code = 'THEMIS'
@@ -17,12 +19,11 @@ location_codes = ['FSIM', 'ATHA', 'TPAS', 'SNKQ']
 map_alt = 110
 min_elevation = 2
 
-ax = asilib.make_map(lon_bounds=(-140, -60), lat_bounds=(40, 82))
+ax = asilib.map.create_map(lon_bounds=(-140, -60), lat_bounds=(40, 82))
 
 for location_code in location_codes:
-    asilib.plot_map(
-        asi_array_code, location_code, time, map_alt, ax=ax, min_elevation=min_elevation
-    )
+    img = asilib.asi.themis.themis(location_code, time=time, alt=map_alt)
+    img.plot_map(ax=ax, min_elevation=min_elevation)
 
 ax.set_title('Donovan et al. 2008 | First breakup of an auroral arc')
 plt.show()
