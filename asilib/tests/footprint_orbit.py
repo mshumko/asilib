19c7b3fb5b57e861fa7853c11b0679b300979c3b
Mykhaylo <msshumko@gmail.com>
2023-04-21 09:46:05 -0400

    Footprint now calculates the times correctly,

--- asilib/tests/footprint_orbit.py
+++ asilib/tests/footprint_orbit.py
@@ -1,10 +1,12 @@
+from datetime import datetime, timedelta
 from typing import Tuple
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 def footprint(center_lon:float, alt:float=110, obit_period:float=95, 
-    precession_rate:float=10, cadence:float=0.1
+    precession_rate:float=10, cadence:float=0.1, 
+    time_range:tuple=(datetime(2015,1,1), datetime(2015,1,2))
     )->Tuple[np.array, np.array]:
     """
     Get the time stamps, and lat, lon, alt (LLA) coordinates.
@@ -21,12 +23,23 @@ def footprint(center_lon:float, alt:float=110, obit_period:float=95,
         Controls how much the longitude change in one orbit.
     cadence:float
         The cadence of the footprint timestamps in minutes.
+    time_range:tuple
+        The start and end times that bound the footprint path.
 
     Returns
     -------
     np.array
-        Footprint timestamps starting on 2010-01-01.
+        Footprint timestamps in datetime.datetime format.
     np.array
-        An array of shape (n_times, 3) with the footprint's
+        An array of floats shaped (n_times, 3) with the footprint's
         lat, lon, alt coordinates.
     """
+    n_minutes = (time_range[1]-time_range[0]).total_seconds()/60
+    n_time_stamps = int(n_minutes/cadence)
+    times = np.array([time_range[0] + timedelta(minutes=i*cadence) for i in range(n_time_stamps)])
+
+    return times
+
+if __name__ == '__main__':
+    times, lla = footprint(-100)
+    pass
