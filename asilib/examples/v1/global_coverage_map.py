80819d5ac338e7e84e8b197f0704ba47cfd7c38a
Mykhaylo Shumko <msshumko@gmail.com>
2023-09-23 11:36:29 -0400

    Removed old comments.

--- asilib/examples/v1/global_coverage_map.py
+++ asilib/examples/v1/global_coverage_map.py
@@ -19,30 +19,13 @@ asi_arrays = {
     }
 
 fig = plt.figure(figsize=(10, 10))
-ax = fig.add_subplot(projection=ccrs.Orthographic(-100, 55))
-# fig.subplots_adjust(bottom=0.05, top=0.95,
-#                     left=0.04, right=0.95, wspace=0.02)
-
-# Limit the map to -60 degrees latitude and below.
-# ax1.set_extent([-180, 180, -90, -60], ccrs.PlateCarree())
+ax = fig.add_subplot(projection=ccrs.Orthographic(-100, 55)))
 
 ax.add_feature(cartopy.feature .LAND, color='g')
 ax.add_feature(cartopy.feature .OCEAN, color='w')
 ax.add_feature(cartopy.feature .COASTLINE, edgecolor='k')
 
 ax.gridlines()
-
-# # Compute a circle in axes coordinates, which we can use as a boundary
-# # for the map. We can pan/zoom as much as we like - the boundary will be
-# # permanently circular.
-# theta = np.linspace(0, 2*np.pi, 100)
-# center, radius = [0.5, 0.5], 0.5
-# verts = np.vstack([np.sin(theta), np.cos(theta)]).T
-# circle = mpath.Path(verts * radius + center)
-
-# ax2.set_boundary(circle, transform=ax2.transAxes)
-
-
 
 for array, (loader, info_df) in asi_arrays.items():
     pass
