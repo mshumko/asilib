40ea113c291fba6390fcd605728349a61bd1cee3
Mike Shumko <mike.shumko@jhuapl.edu>
2023-10-10 14:48:30 -0400

    Refactored the test_geodetic_skymap() test to test the custom_alt kwarg.

--- asilib/tests/test_skymap.py
+++ asilib/tests/test_skymap.py
@@ -1,5 +1,4 @@
 import asilib.asi
-import asilib.skymap
 
 import matplotlib.testing.decorators
 import matplotlib.pyplot as plt
@@ -15,18 +14,12 @@ def test_geodetic_skymap():
     Compare the (lat, lon) skymaps between the asilib and the official implementation.
     """
     time = '2020-01-01'
-    asi = asilib.asi.themis('GILL', time=time, load_images=False)
-    
-    asilib_lat_skymap, asilib_lon_skymap = asilib.skymap.geodetic_skymap(
-        (asi.meta['lat'], asi.meta['lon'], asi.meta['alt']),
-        asi.skymap['az'], asi.skymap['el'], 110
-    )
+    ref_asi = asilib.asi.themis('GILL', time=time, load_images=False, alt=110)
+    asi = asilib.asi.themis('GILL', time=time, load_images=False, custom_alt=True, alt=110)
+
     fig, ax = plt.subplots(2, sharex=True, sharey=True, figsize=(4, 8))
-
-    _ref_lon_skymap, _ref_lat_skymap, _= asi._mask_low_horizon(
-        asi.skymap['lon'], asi.skymap['lat'], asi.skymap['el'], 1E-9
+    ref_asi._pcolormesh_nan(
+        ref_asi.skymap['lon'], ref_asi.skymap['lat'], ref_asi.skymap['lat'], ax[0]
         )
-
-    asi._pcolormesh_nan(_ref_lon_skymap, _ref_lat_skymap, _ref_lat_skymap, ax[0])
-    asi._pcolormesh_nan(asilib_lon_skymap, asilib_lat_skymap, asilib_lat_skymap, ax[1])
+    asi._pcolormesh_nan(asi.skymap['lon'], asi.skymap['lat'], asi.skymap['lat'], ax[1])
     return
