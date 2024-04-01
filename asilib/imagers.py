9b30d7158bb7e696ef3638326960acafdbadbd89
Mike Shumko <mike.shumko@jhuapl.edu>
2024-03-31 20:21:40 -0400

    Cleaning up example.

--- asilib/imagers.py
+++ asilib/imagers.py
@@ -627,10 +627,11 @@ if __name__ == '__main__':
     for guide_time, asi_times, asi_images, ax in gen:
         if '_text_obj' in locals():
             _text_obj.remove()
-        info_str = f'Guide time: {guide_time: %Y:%m:%d %H%M%S}\n'
-        # The imagers and asi_times can be indexed the same way.
+        info_str = f'Guide: {guide_time: %Y:%m:%d %H:%M:%S}\n'
+        # The below for loop is possible because the imagers and 
+        # asi_times can be indexed together.
         for _imager, _imager_time in zip(asis.imagers, asi_times):
-            info_str += f'{_imager.meta["location"]}: {_imager_time: %Y:%m:%d %H%M%S}\n'
+            info_str += f'{_imager.meta["location"]}: {_imager_time: %Y:%m:%d %H:%M:%S}\n'
 
         _text_obj = ax.text(
             0.01, 0.99, info_str, va='top', transform=ax.transAxes, 
