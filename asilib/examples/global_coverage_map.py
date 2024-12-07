8315fcc139210391815e544cbbd2a7c2d4f315a7
Mykhaylo Shumko <msshumko@gmail.com>
2024-12-07 18:23:22 -0500

    Updated global coverage map.

--- asilib/examples/global_coverage_map.py
+++ asilib/examples/global_coverage_map.py
@@ -12,27 +12,27 @@ import cartopy.crs as ccrs
 import cartopy.feature
 
 import asilib.asi
-import asilib.asi.mango
+from asilib.imager import Skymap_Cleaner
 
 asi_arrays = {
-    'THEMIS':(asilib.asi.themis, asilib.asi.themis_info, 'k', (10, 11)),
-    'REGO':(asilib.asi.rego, asilib.asi.rego_info, 'pink', (8, 9)),
-    'TREx-NIR':(asilib.asi.trex_nir, asilib.asi.trex_nir_info, 'c', (12, 13)),
-    'TREx-RGB':(asilib.asi.trex_rgb, asilib.asi.trex_rgb_info, 'purple', (14, 15)),
+    'THEMIS':(asilib.asi.themis, asilib.asi.themis_info, 'k', 10),
+    'REGO':(asilib.asi.rego, asilib.asi.rego_info, 'pink', 10),
+    'TREx-NIR':(asilib.asi.trex_nir, asilib.asi.trex_nir_info, 'c', 12),
+    'TREx-RGB':(asilib.asi.trex_rgb, asilib.asi.trex_rgb_info, 'purple', 15),
     }
 
 mango_sations = [
-    ['CVO', datetime(2024, 10, 25, 5, 28), 'r', (15, 16)],
-    ['CFS', datetime(2024, 10, 25, 5, 28), 'r', (15, 16)],
-    ['MTO', datetime(2024, 10, 25, 5, 28), 'r', (15, 16)],
-    ['EIO', datetime(2024, 10, 25, 5, 28), 'r', (15, 16)],
-    ['PAR', datetime(2024, 10, 25, 5, 28), 'r', (15, 16)]
+    ['CVO', datetime(2024, 10, 25, 5, 28), 'r'],
+    ['CFS', datetime(2024, 10, 25, 5, 28), 'r'],
+    ['MTO', datetime(2024, 10, 25, 5, 28), 'r'],
+    ['EIO', datetime(2024, 10, 25, 5, 28), 'r'],
+    ['PAR', datetime(2024, 10, 25, 5, 28), 'r']
 ]
 
 time = datetime(2022, 1, 1)
 
-fig = plt.figure(figsize=(10, 10))
-ax = fig.add_subplot(projection=ccrs.Orthographic(-100, 55))
+fig = plt.figure(figsize=(10, 8))
+ax = fig.add_subplot(projection=ccrs.Orthographic(-100, 50))
 
 ax.add_feature(cartopy.feature .LAND, color='g')
 ax.add_feature(cartopy.feature .OCEAN, color='w')
@@ -41,25 +41,13 @@ ax.add_feature(cartopy.feature .COASTLINE, edgecolor='k')
 ax.gridlines()
 
 for mango_station in mango_sations:
-    asi = asilib.asi.mango.mango(mango_station[0], 'redline', time=mango_station[1])
+    asi = asilib.asi.mango(mango_station[0], 'redline', time=mango_station[1])
 
-    c = np.ones_like(asi.skymap['lat'])
-    c[
-        (asi.skymap['el'] < mango_station[3][0]) |
-        (asi.skymap['el'] > mango_station[3][1]) | 
-        np.isnan(asi.skymap['el'])
-        ] = np.nan
-    asi._pcolormesh_nan(
-        asi.skymap['lon'],
-        asi.skymap['lat'],
-        c,
-        ax,
-        cmap=matplotlib.colors.ListedColormap([mango_station[2]]),
-        pcolormesh_kwargs={
-            'transform':ccrs.PlateCarree(),
-        }
-    )
-ax.text(0, 4/30, 'MANGO', va='bottom', color=mango_station[2], transform=ax.transAxes, fontsize=20)
+    ax.contour(
+        asi.skymap['lon'], asi.skymap['lat'], asi.skymap['el'], levels=[16],
+        transform=ccrs.PlateCarree(), colors=mango_station[2]
+        )
+ax.text(0, 4/30, 'MANGO-redline', va='bottom', color=mango_station[2], transform=ax.transAxes, fontsize=20)
 
 for i, (array, (loader, info_df, color, elevation_range)) in enumerate(asi_arrays.items()):
     asi_array_info = info_df()
@@ -72,25 +60,19 @@ for i, (array, (loader, info_df, color, elevation_range)) in enumerate(asi_array
                 continue
             else:
                 raise
-        c = np.ones_like(asi.skymap['lat'][:-1, :-1])
-        c[
-            (asi.skymap['el'] < elevation_range[0]) |
-            (asi.skymap['el'] > elevation_range[1]) | 
-            np.isnan(asi.skymap['el'])
-            ] = np.nan
-        asi._pcolormesh_nan(
-            asi.skymap['lon'],
-            asi.skymap['lat'],
-            c,
-            ax,
-            cmap=matplotlib.colors.ListedColormap([color]),
-            pcolormesh_kwargs={
-                'transform':ccrs.PlateCarree(),
-            }
+        _skymap_cleaner = Skymap_Cleaner(
+                asi.skymap['lon'], 
+                asi.skymap['lat'], 
+                asi.skymap['el'],
+            )
+        _lon, _lat = _skymap_cleaner.remove_nans()
+        ax.contour(
+            _lon[:-1, :-1], _lat[:-1, :-1], asi.skymap['el'], levels=[10],
+            transform=ccrs.PlateCarree(), colors=color
         )
         ax.text(0, i/30, array, va='bottom', color=color, transform=ax.transAxes, fontsize=20)
 
-ax.text(0.99, 0, f'Generated on {datetime.now().date()}', va='top', ha='right', transform=ax.transAxes, fontsize=15)
-ax.set_title('Spatial Coverage of Imagers Supported by asilib', fontsize=25)        
+# ax.text(0.99, 0.02, f'As of {datetime.now().date()}', va='bottom', ha='right', transform=ax.transAxes, fontsize=15)
+ax.set_title(f'Imagers Supported by asilib as of {datetime.now().date()}', fontsize=25)        
 plt.tight_layout()
 plt.show()
