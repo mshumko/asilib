05adbb25b376aa5d9f55bdd25b5ce808b155f774
Mykhaylo Shumko <msshumko@gmail.com>
2025-08-03 14:54:00 -0400

    Typo.

--- asilib/asi/__init__.py
+++ asilib/asi/__init__.py
@@ -2,7 +2,7 @@ from .rego import rego, rego_info, rego_skymap
 from .themis import themis, themis_info, themis_skymap
 from .trex import trex_nir, trex_nir_skymap, trex_nir_info
 from .trex import trex_rgb, trex_rgb_info, trex_rgb_skymap
-from .psa_emccd import psa_emccd, psa_emccd_info, psa_emccd_skymapfrom 
+from .psa_emccd import psa_emccd, psa_emccd_info, psa_emccd_skymap 
 from .mango import mango, mango_info
 
 __all__ = [
