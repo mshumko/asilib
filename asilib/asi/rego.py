75a2ad2fe1379df44a6a21417f7b7a4f803509c2
Mykhaylo Shumko <msshumko@gmail.com>
2025-10-04 16:26:30 -0400

    Adapted the TREx, THEMIS, and REGO data loaders so that that numpy < 2.0 requirement can be lifted.

--- asilib/asi/rego.py
+++ asilib/asi/rego.py
@@ -10,12 +10,14 @@ import pathlib
 import copy
 import os
 import dateutil.parser
+import gzip
+import signal
+from multiprocessing import Pool
 
 import numpy as np
 import pandas as pd
 import scipy.io
 import matplotlib.colors
-import rego_imager_readfile
 
 import asilib
 import asilib.asi.themis as themis
@@ -362,7 +364,7 @@ def _tranform_longitude_to_180(skymap):
 
 
 def _load_rego_pgm(path):
-    images, meta, problematic_file_list = rego_imager_readfile.read(str(path))
+    images, meta, problematic_file_list = read(str(path))
     if len(problematic_file_list):
         raise ValueError(f'A problematic PGM file: {problematic_file_list[0]}')
     images = np.moveaxis(images, 2, 0)
@@ -383,3 +385,318 @@ def _glob_filename(time, location_code, _, dark=False):
         return f'{time.strftime("%Y%m%d_%H%M")}_{location_code.lower()}_rego*6300_dark.pgm.gz'
     else:
         return f'{time.strftime("%Y%m%d_%H%M")}_{location_code.lower()}_rego*6300.pgm.gz'
+
+
+"""
+TThe data reader below was developed by the University of Calgary under the 
+MIT License. You can find the original source files at:  
+https://github.com/ucalgary-srs/rego-imager-readfile
+
+MIT License
+
+Copyright (c) 2020 University of Calgary
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+# globals
+REGO_EXPECTED_HEIGHT = 512
+REGO_EXPECTED_WIDTH = 512
+REGO_DT = np.dtype("uint16")
+REGO_DT = REGO_DT.newbyteorder('>')  # force big endian byte ordering
+
+
+def read(file_list, workers=1, first_frame=False, no_metadata=False, quiet=False):
+    """
+    Read in a single PGM file or set of PGM files
+
+    :param file_list: filename or list of filenames
+    :type file_list: str
+    :param workers: number of worker processes to spawn, defaults to 1
+    :type workers: int, optional
+    :param first_frame: only read the first frame for each file, defaults to False
+    :type first_frame: bool, optional
+    :param no_metadata: exclude reading of metadata (performance optimization if
+                        the metadata is not needed), defaults to False
+    :type no_metadata: bool, optional
+    :param quiet: reduce output while reading data
+    :type quiet: bool, optional
+
+    :return: images, metadata dictionaries, and problematic files
+    :rtype: numpy.ndarray, list[dict], list[dict]
+    """
+    # if input is just a single file name in a string, convert to a list to be fed to the workers
+    if isinstance(file_list, str):
+        file_list = [file_list]
+
+    # check workers
+    if (workers > 1):
+        try:
+            # set up process pool (ignore SIGINT before spawning pool so child processes inherit SIGINT handler)
+            original_sigint_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
+            pool = Pool(processes=workers)
+            signal.signal(signal.SIGINT, original_sigint_handler)  # restore SIGINT handler
+        except ValueError:
+            # likely the read call is being used within a context that doesn't support the usage
+            # of signals in this way, proceed without it
+            pool = Pool(processes=workers)
+
+        # call readfile function, run each iteration with a single input file from file_list
+        # NOTE: structure of data - data[file][metadata dictionary lists = 1, images = 0][frame]
+        data = []
+        try:
+            data = pool.map(functools.partial(
+                __rego_readfile_worker,
+                first_frame=first_frame,
+                no_metadata=no_metadata,
+                quiet=quiet,
+            ), file_list)
+        except KeyboardInterrupt:
+            pool.terminate()  # gracefully kill children
+            return np.empty((0, 0, 0), dtype=REGO_DT), [], []
+        else:
+            pool.close()
+            pool.join()
+    else:
+        # don't bother using multiprocessing with one worker, just call the worker function directly
+        data = []
+        for f in file_list:
+            data.append(__rego_readfile_worker(
+                f,
+                first_frame=first_frame,
+                no_metadata=no_metadata,
+                quiet=quiet,
+            ))
+
+    # derive number of frames to prepare for
+    total_num_frames = 0
+    image_height = REGO_EXPECTED_HEIGHT
+    image_width = REGO_EXPECTED_WIDTH
+    for i in range(0, len(data)):
+        if (data[i][2] is True):
+            continue
+        total_num_frames += data[i][0].shape[2]
+        image_height = data[i][0].shape[0]
+        image_width = data[i][0].shape[1]
+
+    # pre-allocate array sizes
+    images = np.empty([image_height, image_width, total_num_frames], dtype=REGO_DT)
+    metadata_dict_list = [{}] * total_num_frames
+    problematic_file_list = []
+
+    # populate data
+    list_position = 0
+    for i in range(0, len(data)):
+        # check if file was problematic
+        if (data[i][2] is True):
+            problematic_file_list.append({
+                "filename": data[i][3],
+                "error_message": data[i][4],
+            })
+            continue
+
+        # check if any data was read in
+        if (len(data[i][1]) == 0):
+            continue
+
+        # find actual number of frames, this may differ from predicted due to dropped frames, end
+        # or start of imaging
+        real_num_frames = data[i][0].shape[2]
+
+        # metadata dictionary list at data[][1]
+        metadata_dict_list[list_position:list_position + real_num_frames] = data[i][1]
+        images[:, :, list_position:list_position + real_num_frames] = data[i][0]  # image arrays at data[][0]
+        list_position = list_position + real_num_frames  # advance list position
+
+    # trim unused elements from predicted array sizes
+    metadata_dict_list = metadata_dict_list[0:list_position]
+    images = np.delete(images, range(list_position, total_num_frames), axis=2)
+
+    # ensure entire array views as uint16
+    images = images.astype(np.uint16)
+
+    # return
+    data = None
+    return images, metadata_dict_list, problematic_file_list
+
+
+def __rego_readfile_worker(file, first_frame=False, no_metadata=False, quiet=False):
+    # init
+    images = np.array([])
+    metadata_dict_list = []
+    is_first = True
+    metadata_dict = {}
+    site_uid = ""
+    device_uid = ""
+    problematic = False
+    error_message = ""
+
+    # check file extension to see if it's gzipped or not
+    try:
+        if file.endswith("pgm.gz"):
+            unzipped = gzip.open(file, mode='rb')
+        elif file.endswith("pgm"):
+            unzipped = open(file, mode='rb')
+        else:
+            if (quiet is False):
+                print("Unrecognized file type: %s" % (file))
+            problematic = True
+            error_message = "Unrecognized file type"
+            try:
+                unzipped.close()
+            except Exception:
+                pass
+            return images, metadata_dict_list, problematic, file, error_message
+    except Exception as e:
+        if (quiet is False):
+            print("Failed to open file '%s' " % (file))
+        problematic = True
+        error_message = "failed to open file: %s" % (str(e))
+        try:
+            unzipped.close()
+        except Exception:
+            pass
+        return images, metadata_dict_list, problematic, file, error_message
+
+    # read the file
+    prev_line = None
+    line = None
+    while True:
+        # break out depending on first_frame param
+        if (first_frame is True and is_first is False):
+            break
+
+        # read a line
+        try:
+            prev_line = line
+            line = unzipped.readline()
+        except Exception as e:
+            if (quiet is False):
+                print("Error reading before image data in file '%s'" % (file))
+            problematic = True
+            metadata_dict_list = []
+            images = np.array([])
+            error_message = "error reading before image data: %s" % (str(e))
+            try:
+                unzipped.close()
+            except Exception:
+                pass
+            return images, metadata_dict_list, problematic, file, error_message
+
+        # break loop at end of file
+        if (line == b''):
+            break
+
+        # magic number; this is not a metadata or image line, exclude
+        if (line.startswith(b'P5\n')):
+            continue
+
+        # process line
+        if (line.startswith(b'#"')):
+            if (no_metadata is True):
+                metadata_dict = {}
+                metadata_dict_list.append(metadata_dict)
+            else:
+                # metadata lines start with #"<key>"
+                try:
+                    line_decoded = line.decode("ascii")
+                except Exception as e:
+                    # skip metadata line if it can't be decoded, likely corrupt file
+                    if (quiet is False):
+                        print("Error decoding metadata line: %s (line='%s', file='%s')" % (str(e), line, file))
+                    problematic = True
+                    error_message = "error decoding metadata line: %s" % (str(e))
+                    continue
+
+                # split the key and value out of the metadata line
+                line_decoded_split = line_decoded.split('"')
+                if (len(line_decoded_split) != 3):
+                    if (quiet is False):
+                        print("Warning: issue splitting metadata line (line='%s', file='%s')" % (line_decoded, file))
+                    continue
+                key = line_decoded_split[1]
+                value = line_decoded_split[2].strip()
+
+                # add entry to dictionary
+                metadata_dict[key] = value
+
+                # set the site/device uids, or inject the site and device UIDs if they are missing
+                if ("Site unique ID" not in metadata_dict):
+                    metadata_dict["Site unique ID"] = site_uid
+                else:
+                    site_uid = metadata_dict["Site unique ID"]
+                if ("Imager unique ID" not in metadata_dict):
+                    metadata_dict["Imager unique ID"] = device_uid
+                else:
+                    device_uid = metadata_dict["Imager unique ID"]
+
+                # split dictionaries up per frame, exposure plus initial readout is
+                # always the end of metadata for frame
+                if (key.startswith("Exposure plus readout")):
+                    metadata_dict_list.append(metadata_dict)
+                    metadata_dict = {}
+        elif line == b'65535\n':
+            # there are 2 lines between "exposure plus read out" and the image
+            # data, the first is the image dimensions and the second is the max
+            # value
+            #
+            # check the previous line to get the dimensions of the image
+            prev_line_split = prev_line.decode("ascii").strip().split()
+            image_width = int(prev_line_split[0])
+            image_height = int(prev_line_split[1])
+            bytes_to_read = image_width * image_height * 2  # 16-bit image depth
+
+            # read image
+            try:
+                # read the image size in bytes from the file
+                image_bytes = unzipped.read(bytes_to_read)
+
+                # format bytes into numpy array of unsigned shorts (2byte numbers, 0-65536),
+                # effectively an array of pixel values
+                image_np = np.frombuffer(image_bytes, dtype=REGO_DT)
+
+                # change 1d numpy array into matrix with correctly located pixels
+                image_matrix = np.reshape(image_np, (image_height, image_width, 1))
+            except Exception as e:
+                if (quiet is False):
+                    print("Failed reading image data frame: %s" % (str(e)))
+                metadata_dict_list.pop()  # remove corresponding metadata entry
+                problematic = True
+                error_message = "image data read failure: %s" % (str(e))
+                continue  # skip to next frame
+
+            # initialize image stack
+            if (is_first is True):
+                images = image_matrix
+                is_first = False
+            else:
+                images = np.dstack([images, image_matrix])  # depth stack images (on 3rd axis)
+
+    # close gzip file
+    unzipped.close()
+
+    # check to see if the image is empty
+    if (images.size == 0):
+        if (quiet is False):
+            print("Error reading image file: found no image data")
+        problematic = True
+        error_message = "no image data"
+
+    # return
+    return images, metadata_dict_list, problematic, file, error_message
